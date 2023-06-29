f2cb52c2a7766aba9f8c0665f20f790fbe80026b
shivamkushwaha2 <73945736+shivamkushwaha2@users.noreply.github.com>
2023-06-29 22:58:47 +0530

    Update Entertainment.kt

--- app/src/main/java/com/example/myapplication/Entertainment.kt
+++ app/src/main/java/com/example/myapplication/Entertainment.kt
@@ -66,7 +66,7 @@ class Entertainment : AppCompatActivity(), NewsItemclicked {
         }
     private fun fetchdata(page:Int) {
         isLoading = true
-        url ="https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=ef78e990b17546c98dec95130aa71ba4&page=$page"
+        url ="https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=&page=$page"
         val jsonObjectRequest = object: JsonObjectRequest(
             Method.GET, url,
             null,
