95c0623c81fbb603513cb5cbacdec1bbe1d2940a
shivamkushwaha2 <73945736+shivamkushwaha2@users.noreply.github.com>
2023-06-29 23:01:29 +0530

    Update PageFragment.kt

--- app/src/main/java/com/example/myapplication/PageFragment.kt
+++ app/src/main/java/com/example/myapplication/PageFragment.kt
@@ -81,7 +81,7 @@ class PageFragment : Fragment() , NewsItemclicked {
     private fun fetchdata(page: Int,btn: LottieAnimationView) {
         isLoading = true
 
-        url ="https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=ef78e990b17546c98dec95130aa71ba4&page=$page"
+        url ="https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=&page=$page"
         val jsonObjectRequest = object: JsonObjectRequest(
             Method.GET, url,
             null,
